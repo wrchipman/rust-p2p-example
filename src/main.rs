b2f6f88b42d0aa99a0a11778cdba1e44be7eb4cc
William Chipman <wrchipman@gmail.com>
2022-01-06 18:28:23 -0300

    added logging and secure keys for traffic

--- src/main.rs
+++ src/main.rs
@@ -39,3 +39,13 @@ enum EventType {
     Input(String),
 }
 
+#[tokio::main]
+async fn main() {
+    pretty_env_logger::init();
+
+    info!("Peer Id: {}", PEER_ID.clone());
+    let (response_sender, mut response_rcv) = mpsc::unbounded_channel();
+
+    let auth_keys = Keypair::<X25519Spec>::new().into_authentic(&KEYS).expect("can create auth keys");
+    
+}
