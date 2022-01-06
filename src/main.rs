72c83d45da2532b7c11594023a85a67c4bf078c0
William Chipman <wrchipman@gmail.com>
2022-01-06 18:14:10 -0300

    Add special types

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+const STORAGE_FILE_PATH: &str = "./recipes.json";
+
+type Result<t> = std::result::Result<T, Box<dyn std:error:Error + Send + Sync + 'static'>>;
+static KEYS: Lazy<identity::Keypair> = Lazy::new(|| identity::Keypair::generate_ed25519());
+static PEER_ID: Lazy<PeerId> = Lazy::new(|| PeerId::from(KEYS.public()));
+static TOPIC: Lazy<Topic> = Lazy::new(|| Topic::new("recipes"));
+
+type Recipes = Vec<Recipe>;
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Recipe {
+    id: usize,
+    name: String,
+    ingredients: String,
+    instructions: String,
+    public: bool,
 }
