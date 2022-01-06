4811acc310f2b4de4e234662b6bfd87f056ce856
William Chipman <wrchipman@gmail.com>
2022-01-06 18:21:13 -0300

    Added message types

--- src/main.rs
+++ src/main.rs
@@ -15,3 +15,27 @@ struct Recipe {
     instructions: String,
     public: bool,
 }
+
+#[derive(Debug, Serialize, Deserialize)]
+enum ListMode {
+    ALL,
+    One(String),
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct ListRequest {
+    mode: ListMode,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct ListResponse { 
+    mode: ListMode,
+    data: Recipes,
+    receiver: String,
+}
+
+enum EventType {
+    Response(ListResponse),
+    Input(String),
+}
+
